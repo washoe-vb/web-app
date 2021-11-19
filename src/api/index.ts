c0e70a92ef3f068ef80c26b9a04699017498ca5b
Herman Streltsov <mail@streltsov.dev>
2021-11-20 02:12:14 +0300

    Add authorization header to axios instance

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,13 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
-export const instance = axios.create({
+const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL
 });
+
+instance.interceptors.request.use(
+  (config: AxiosRequestConfig) =>
+    ({ ...config, headers: { ...config.headers, authorization: localStorage.getItem("token") } }),
+  error => Promise.reject(error)
+);
+
+export { instance };
