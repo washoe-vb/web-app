95b91a3a581d0155d9c6ab09ea24937290d53896
Herman Streltsov <mail@streltsov.dev>
2022-07-01 14:16:29 +0300

    Create getWordMeaning query

---
+++ src/api/queries/getWordMeaning.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+
+const engDictionaryPath = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+const getWordMeaning = (word: string) =>
+  axios.get(engDictionaryPath + word)
+    .then(({ data }) => data);
+
+export const useWordMeaning = (word: string) =>
+  useQuery([ "get-meaning", word ], () => getWordMeaning(word), { enabled: Boolean(word) });
