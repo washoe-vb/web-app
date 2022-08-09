689e857ff58834ce5e8fc81e66f768a1ce0bb7b7
Herman Streltsov <mail@streltsov.dev>
2022-08-09 11:22:41 +0300

    Add query string to from url

--- src/containers/Login/Login.tsx
+++ src/containers/Login/Login.tsx
@@ -3,17 +3,15 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { LoginForm } from "components/LoginForm";
 import { useAuth } from "hooks/use-auth";
 
-function useRedirectToRequestedPage (isAuthenticated: boolean) {
-  const location = useLocation();
+function useRedirectToRequestedPage(isAuthenticated: boolean) {
+  const { state } = useLocation();
   const navigate = useNavigate();
-  const from = location.state?.from?.pathname || "/";
+  const from = state?.from?.pathname + state?.from?.search || "/";
 
   useEffect(() => {
     if (isAuthenticated) navigate(from, { replace: true });
-  }, [ isAuthenticated, from, navigate ]);
+  }, [isAuthenticated, from, navigate]);
 }
-
-
 
 export const Login = () => {
   const { logIn, isAuthenticated } = useAuth();
