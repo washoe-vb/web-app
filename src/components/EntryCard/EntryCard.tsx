354bf2e572da4a65265fdc5f6396afad6a78bfb0
Herman Streltsov <mail@streltsov.dev>
2022-08-10 22:30:35 +0300

    Create EntryCard component

---
+++ src/components/EntryCard/EntryCard.tsx
@@ -0,0 +1,55 @@
+import { VFC } from "react";
+import { Link as RouterLink } from "react-router-dom";
+import {
+  Box,
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from "@mui/material";
+type DefinitionTypes = {
+  word: string;
+  definition: string;
+  synonyms: string[];
+  example: undefined | string;
+};
+
+export const EntryCard: VFC<DefinitionTypes> = ({
+  word,
+  definition,
+  example,
+  synonyms,
+}) => {
+  const formattedSynonyms = Boolean(synonyms.length)
+    ? " : " + JSON.stringify(synonyms)
+    : "";
+  const formattedDefinition = `: ${definition.toLowerCase()}${formattedSynonyms}`;
+  const link = `/add-word?word=${word}&definition=${
+    formattedDefinition || ""
+  }&example=${example || ""}`;
+
+  return (
+    <Box sx={{ mb: 1 }}>
+      <Card variant="outlined">
+        <CardContent>
+          <Typography variant="body1">{formattedDefinition}</Typography>
+          {example && (
+            <Typography sx={{ mb: 1.5 }} variant="body2" color="text.secondary">
+              <ul>
+                <li>
+                  <i>{example}</i>
+                </li>
+              </ul>
+            </Typography>
+          )}
+        </CardContent>
+        <CardActions>
+          <Button component={RouterLink} to={link} size="large">
+            Add
+          </Button>
+        </CardActions>
+      </Card>
+    </Box>
+  );
+};
---
+++ src/components/EntryCard/index.ts
@@ -0,0 +1,1 @@
+export { EntryCard } from "./EntryCard";
