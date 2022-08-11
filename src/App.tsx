c210a0ef1f7837d42d33cea2a9c84b0cdf3ccf4f
Herman Streltsov <mail@streltsov.dev>
2022-08-11 09:55:10 +0300

    Change route word-list to my-words

--- src/App.tsx
+++ src/App.tsx
@@ -75,7 +75,7 @@ export const App = () => {
                 }
               />
               <Route
-                path="/words-list"
+                path="/my-words"
                 element={
                   <RequireAuth>
                     <WordsList />
