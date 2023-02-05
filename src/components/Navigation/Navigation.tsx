1579c87714a92b5c31a4026333ef95bdb859b400
Herman Streltsov <mail@streltsov.dev>
2023-02-05 15:42:14 +0100

    Fix Navigation link's color

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 export const Navigation = () => (
   <AppBar position="fixed" component="nav">
     <Toolbar>
-      <Button component={Link} to="/">
+      <Button color="inherit" component={Link} to="/">
         Washoe
       </Button>
       <Box ml="auto" display="flex" gap={1}>
-        <Button component={Link} to="/add-word">
+        <Button color="inherit" component={Link} to="/add-word">
           Add Word
         </Button>
-        <Button component={Link} to="/my-words">
+        <Button color="inherit" component={Link} to="/my-words">
           My Words
         </Button>
       </Box>
